ad7adc49bba1e999ec146d131b7495fdac2d6612
brianna alora <me@bria.moe>
2022-09-24 21:33:24 -0400

    switch from string args to any type args

--- sanitize.go
+++ sanitize.go
@@ -1,16 +1,76 @@
 package planetscale
 
 import (
+	"encoding/hex"
 	"errors"
+	"strconv"
 	"strings"
+	"time"
 )
 
-func sanitize(query string, args []string) (string, error) {
+func sanitize(query string, args []any) (string, error) {
 	if strings.Count(query, "?") != len(args) {
 		return "", errors.New("invalid amount of query args")
 	}
 
-	// escape important values
+	var f strings.Builder
+	i := 0
+
+	for _, c := range query {
+		if c != '?' {
+			f.WriteRune(c)
+			continue
+		}
+
+		if err := sanitizeArg(args[i], &f); err != nil {
+			return "", err
+		}
+
+		i++
+	}
+
+	return f.String(), nil
+}
+
+func sanitizeArg(arg any, f *strings.Builder) error {
+	switch a := arg.(type) {
+	case nil:
+		f.WriteString("null")
+	case bool:
+		f.WriteString(strconv.FormatBool(a))
+	case string:
+		f.WriteRune('\'')
+		f.WriteString(escapeString(a))
+		f.WriteRune('\'')
+	case int:
+		f.WriteString(strconv.FormatInt(int64(a), 10))
+	case int8:
+		f.WriteString(strconv.FormatInt(int64(a), 10))
+	case int16:
+		f.WriteString(strconv.FormatInt(int64(a), 10))
+	case int32:
+		f.WriteString(strconv.FormatInt(int64(a), 10))
+	case int64:
+		f.WriteString(strconv.FormatInt(a, 10))
+	case float32:
+		f.WriteString(strconv.FormatFloat(float64(a), 'f', -1, 32))
+	case float64:
+		f.WriteString(strconv.FormatFloat(a, 'f', -1, 64))
+	case time.Time:
+		f.WriteString(a.UTC().Format("2006-01-02T15:04:05.999Z07:00"))
+	case []byte:
+		f.WriteString(`'\x`)
+		f.WriteString(hex.EncodeToString(a))
+		f.WriteRune('\'')
+	default:
+		return errors.New("invalid type passed to query args")
+	}
+
+	return nil
+}
+
+func escapeString(input string) string {
+	// escapes important values
 	r := strings.NewReplacer(
 		"\"", "\\\"",
 		"'", "\\'",
@@ -23,21 +83,5 @@ func sanitize(query string, args []string) (string, error) {
 		"\x1a", "\\Z",
 	)
 
-	var f strings.Builder
-	i := 0
-
-	for _, c := range query {
-		if c != '?' {
-			f.WriteRune(c)
-			continue
-		}
-
-		f.WriteRune('\'')
-		f.WriteString(r.Replace(args[i]))
-		f.WriteRune('\'')
-
-		i++
-	}
-
-	return f.String(), nil
+	return r.Replace(input)
 }
--- sanitize_test.go
+++ sanitize_test.go
@@ -5,30 +5,37 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestSanitize(t *testing.T) {
 	query := "SELECT * FROM test WHERE id=?"
-	args := map[string]string{
-		"\"":   "\\\"",
-		"'":    "\\'",
-		"\n":   "\\n",
-		"\r":   "\\r",
-		"\t":   "\\t",
-		"\\":   "\\\\",
-		"\x00": "\\0",
-		"\b":   "\\b",
-		"\x1a": "\\Z",
+	args := map[any]string{
+		nil:         "null",
+		true:        "true",
+		1:           "1",
+		1.1:         "1.1",
+		time.Time{}: time.Time{}.Format("2006-01-02T15:04:05.999Z07:00"),
+
+		"\"":   "'\\\"'",
+		"'":    "'\\''",
+		"\n":   "'\\n'",
+		"\r":   "'\\r'",
+		"\t":   "'\\t'",
+		"\\":   "'\\\\'",
+		"\x00": "'\\0'",
+		"\b":   "'\\b'",
+		"\x1a": "'\\Z'",
 	}
 
 	for k, v := range args {
 		t.Run(fmt.Sprintf("sanitize %s", k), func(t *testing.T) {
-			sanitized, err := sanitize(query, []string{k})
+			sanitized, err := sanitize(query, []any{k})
 			if err != nil {
 				t.Fatal(err)
 			}
 
-			want := fmt.Sprintf("SELECT * FROM test WHERE id='%s'", v)
+			want := fmt.Sprintf("SELECT * FROM test WHERE id=%v", v)
 
 			if !strings.EqualFold(sanitized, want) {
 				t.Fatalf("wanted %s, but got %s", want, sanitized)
@@ -38,7 +45,7 @@ func TestSanitize(t *testing.T) {
 
 	t.Run("arg length", func(t *testing.T) {
 		e := errors.New("invalid amount of query args")
-		_, err := sanitize(query, []string{"", ""})
+		_, err := sanitize(query, []any{"", ""})
 
 		if err.Error() != e.Error() {
 			t.Fatalf("wanted %s, but got %s", e, err)
